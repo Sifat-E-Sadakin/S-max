308387f9619f6c2f7fe6aa456735bbdcdc8eaf23
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2024-02-18 23:12:57 +0600

    new ui

--- app/components/block/Timer.jsx
+++ app/components/block/Timer.jsx
@@ -1,12 +1,10 @@
 "use client";
-import styled from "styled-components";
 import Countdown from "react-countdown";
-import { useEffect } from "react";
 
 const Timer = () => {
   const renderer = ({ days, hours, minutes, seconds }) => {
     return (
-      <Times>
+      <section className="flex gap-8 ">
         <div>
           <h2>{days}</h2>
           <p>Days</p>
@@ -23,55 +21,18 @@ const Timer = () => {
           <h2>{seconds}</h2>
           <p>Seconds</p>
         </div>
-      </Times>
+      </section>
     );
   };
-  useEffect(() => {}, []);
+
   return (
-    <section>
-      <TimerDiv className="timer">
-        <TimeHeading>Next Course starts in:</TimeHeading>
+    <section className="container mx-auto my-10">
+      <div className="timer flex justify-center items-center gap-7 ">
+        <h1 className="text-2xl font-semibold">Next Course starts in:</h1>
         <Countdown date={Date.now() + 2764800000} renderer={renderer} />
-      </TimerDiv>
+      </div>
     </section>
   );
 };
 
 export default Timer;
-
-const TimerDiv = styled.div`
-  height: 132px;
-  width: 856px;
-  background-image: linear-gradient(#827bf5, #e15ef7);
-  margin: 0 auto;
-  border-radius: 18px;
-  color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  @media screen and (max-width: 992px) {
-    width: 90%;
-    height: 60px;
-  }
-`;
-const TimeHeading = styled.h2`
-  font-size: calc(18px + (40 - 18) * ((100vw - 360px) / (1920 - 360)));
-  font-weight: 700;
-`;
-const Times = styled.div`
-  display: flex;
-  div {
-    text-align: center;
-    margin-left: 25px;
-    h2 {
-      font-size: calc(18px + (45 - 18) * ((100vw - 360px) / (1920 - 360)));
-      font-weight: 700;
-    }
-    p {
-      font-size: calc(6px + (18 - 6) * ((100vw - 360px) / (1920 - 360)));
-    }
-    @media screen and (max-width: 992px) {
-      margin-left: 15px;
-    }
-  }
-`;
--- app/components/ui/navbar.jsx
+++ app/components/ui/navbar.jsx
@@ -1,206 +1,82 @@
-"use client";
-import styled from "styled-components";
-import logo from "@/public/img/logo.png";
-import twitter from "@/public/img/twitter.png";
-import paper from "@/public/img/paper.png";
-import youtube from "@/public/img/yt.png";
-import instagram from "@/public/img/instagram.png";
-import discord from "@/public/img/discord.png";
-import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-const Navbar = () => {
-  const [openNav, setOpenNav] = useState(false);
-  useEffect(() => {
-    openNav
-      ? ((document.body.style.overflow = "hidden"),
-        (document.body.style.position = "fixed"),
-        (document.body.style.width = "100%"),
-        (document.body.style.height = "100%"))
-      : ((document.body.style.overflow = "visible"),
-        (document.body.style.position = "static"),
-        (document.body.style.width = "auto"),
-        (document.body.style.height = "auto"));
-  });
+import React from "react";
+
+const navbar = () => {
   return (
-    <Header className="header">
-      <>
-        <Nav>
-          <Logo>
-            <Link className="font-bold text-5xl" href="/">
-              SoftMax
-            </Link>
-          </Logo>
-          <Menu openNav={openNav}>
-            <List>
-              <a href="#">Our Idea</a>
-            </List>
-            <List>
-              <a href="#">InfiniteLaunch</a>
-            </List>
-            <List>
-              <a href="#">$Infinity</a>
-            </List>
-            <List>
-              <a href="#">Audits</a>
-            </List>
-            <List>
-              <a href="#">Whitepaper</a>
-            </List>
-            <List type={"social"}>
-              <a
-                href="https://www.youtube.com/channel/UCOCFFrKsRMtbnnQtnQLsSyw"
-                target="_blank"
-                rel="noreferrer">
-                <Image src={youtube} alt="youtube" />
-              </a>
-              <a href="https://twitter.com/infinite_eco" className="ms-3">
-                <Image src={twitter} alt="twitter" />
-              </a>
-              <a
-                href="https://www.instagram.com/ecosysteminfinite/"
-                className="ms-3">
-                <Image src={instagram} alt="instagram" width={19} height={19} />
-              </a>
-              <a href="https://t.me/InfiniteTG" className="ms-3">
-                <Image src={paper} alt="Paper" />
-              </a>
-              <a href="https://discord.gg/y7uvFsGY" className="ms-3">
-                <Image src={discord} alt="Discord" width={19} height={19} />
-              </a>
-            </List>
-            <List className="btn btn-primary" type="btn">
-              <Link href="/login" className="grad-btn" rel="noreferrer">
-                Login
-              </Link>
-            </List>
-            <List>
-              <div className="social-btn">
-                <a
-                  href="https://drive.google.com/file/d/1J5dDsadAeVMzYmU9bVgS8argBHndkCIM/view?usp=sharing"
-                  className="outline-btn"
-                  target="_blank"
-                  rel="noreferrer">
-                  Whitepaper (EN)
-                </a>
-                <a
-                  href="https://drive.google.com/file/d/1Ws2QIA-MbQMGsa74rCfk9P9s20pAEBWk/view?usp=sharing"
-                  className="grad-btn"
-                  target="_blank"
-                  rel="noreferrer">
-                  Whitepaper (JN)
-                </a>
-              </div>
-            </List>
-          </Menu>
-          <Toggle>
-            {openNav ? (
-              <AiOutlineClose onClick={() => setOpenNav(false)} />
-            ) : (
-              <AiOutlineMenu onClick={() => setOpenNav(true)} />
-            )}
-          </Toggle>
-        </Nav>
-      </>
-    </Header>
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <a href="/" className="btn btn-ghost text-xl">
+          SoftMax
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Link className="btn btn-outline" href="/login">
+          Login
+        </Link>
+      </div>
+    </div>
   );
 };
 
-export default Navbar;
-
-const Header = styled.header`
-  background-color: white;
-`;
-
-const Nav = styled.nav`
-  height: 150px;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  @media screen and (max-width: 1400px) {
-    height: 100px;
-  }
-`;
-
-const Logo = styled.div`
-  width: 180px;
-  @media screen and (max-width: 1400px) {
-    width: 120px;
-  }
-`;
-
-const Menu = styled.ul`
-  display: flex;
-  align-items: center;
-  @media screen and (max-width: 1400px) {
-    position: fixed;
-    width: 100%;
-    height: 89vh;
-    top: 74px;
-    right: ${props => (props.openNav ? 0 : "-100%")};
-    background-color: white;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    transition: 0.5s;
-    z-index: 999;
-  }
-`;
-const List = styled.li`
-  display: flex;
-  align-items: center;
-  margin-left: ${props => {
-    switch (props.type) {
-      case "social":
-        return "86px";
-      case "btn":
-        return "34.5px";
-      default:
-        return "50px";
-    }
-  }};
-  a {
-    color: var(--black);
-    display: inline-block;
-  }
-  .grad-btn {
-    color: white;
-  }
-  .social-btn {
-    display: flex;
-    width: 100%;
-    display: none;
-    .outline-btn {
-      background-color: white;
-      margin-right: 10px;
-    }
-    .grad-btn {
-      &:hover {
-        background-image: none;
-        background-color: white;
-        background-clip: border-box;
-        -webkit-text-fill-color: black;
-      }
-    }
-  }
-  @media screen and (max-width: 1400px) {
-    margin: 15px 0px;
-    .social-btn {
-      display: block;
-    }
-  }
-`;
-
-const Toggle = styled.div`
-  color: var(--black);
-  font-size: 32px;
-  cursor: pointer;
-  display: none;
-  @media screen and (max-width: 1400px) {
-    display: block;
-    position: relative;
-    bottom: 6px;
-  }
-`;
+export default navbar;
